ede04fa4115977bb398b45723ba30199171530c9
Alejandro.Castillo <alejandro.castillo@fluxit.com.ar>
2021-08-30 09:54:24 -0300

    login vc added

---
+++ MyLoginModule/Classes/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  MyLoginModule
+//
+//  Created by Alejandro Castillo on 30/08/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
